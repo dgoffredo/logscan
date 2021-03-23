22ff4303e0555c69443c664871424282ee78223c
David Goffredo <dmgoffredo@gmail.com>
2021-03-22 18:20:56 -0600

    I'M ON AN AIRPLANE

--- alerts/total_volume.py
+++ alerts/total_volume.py
@@ -7,7 +7,7 @@ import .alert
 _sql_template =  """
 -- the timestamp at the beginning of the window
 select max(unix_time)
-    where unix_time <= (select max(unix_time) from Events) - {}
+    where unix_time <= (select max(unix_time) from Events) - {seconds}
 
 union all
 
@@ -21,7 +21,7 @@ select count(*)
 from Events
 where unix_time >=
     (select max(unix_time)
-     where unix_time <= (select max(unix_time) from Events) - {})
+     where unix_time <= (select max(unix_time) from Events) - {seconds})
 """
 
 
@@ -29,7 +29,7 @@ class TotalVolume(alert.Alert):
     def __init__(self, window_seconds, volume_threshold):
         self._window_seconds = window_seconds
         self._volume_threshold = volume_threshold
-        self._sql = _sql_template.format(window_seconds)
+        self._sql = _sql_template.format(seconds=window_seconds)
 
     def window_seconds(self):
         return self._window_seconds
@@ -44,12 +44,8 @@ class TotalVolume(alert.Alert):
         scaled_count = window_seconds / (end_time - begin_time) * count
         
         if scaled_count <= threshold:
-            # It's not necessarily a recovery, but each time we are below the
-            # threshold, we produce a message that says we recovered.  The
-            # calling code will use the message only if we were previously
-            # triggered.
-            recovery = f'total volume of requests alert recovered: fell below {threshold} ({scaled_count}) on average over the {window_seconds} seconds preceding Unix time {end_time}'
-            return alert.Status(triggered=False, message=recovery)
+            message = f'total volume of requests was at or below {threshold} ({scaled_count}) on average over the {window_seconds} seconds preceding Unix time {end_time}'
+            return alert.Status(triggered=False, message=message)
         
-        message = f'total volume of requests alert triggered: exceeds limit of {threshold} ({scaled_count}) on average over the {window_seconds} seconds preceding Unix time {end_time}'
+        message = f'total volume of requests exceeded limit of {threshold} ({scaled_count}) on average over the {window_seconds} seconds preceding Unix time {end_time}'
         return alert.Status(triggered=True, message=message)
--- monitor.py
+++ monitor.py
@@ -2,6 +2,7 @@
 
 
 import alerts
+import event
 import statistics
 import dataclasses
 import sqlite3
@@ -10,29 +11,65 @@ from typing import Optional
 
 @dataclasses.dataclass
 class AlertState:
-    triggered: bool
+    name: str
     alert: alerts.Alert
+    triggered: bool
 
 
 @dataclasses.dataclass
 class StatisticState:
-    last_run_unix_time: Optional[int]
+    name: str
     statistic: statistics.Statistic
+    last_run_unix_time: Optional[int]
 
 
 class Monitor:
     def __init__(self, alerts, statistics, on_notice):
-        self.alerts = [AlertState(triggered=False, alert=alert) for alert in alerts]
-        self.statistics = [StatisticState(last_run_unix_time=None, statistic=stat) for stat in statistics]
+        self.alerts = [AlertState(name, alert, triggered=False) for name, alert in alerts.items()]
+        self.statistics = [StatisticState(name, statistic, last_run_unix_time=None) for name, statistic in statistics.items()]
         self.on_notice = on_notice
         self.db = setup_database()
+        # Keep enough events (a window) to satisfy the alert/statistic having the widest window.
+        self.window_seconds = max(
+            *(state.alert.window_seconds() for state in self.alerts),
+            *(state.statistic.period_seconds() for state in self.statistics))
 
-    def handle_event(self, event):
-        pass # TODO
+    def handle_event(self, event: event.Event):
+        insert_event(self.db, event)
+
+        for state in self.alerts:
+            pass # TODO
+
+        for state in self.statistics:
+            pass # TODO
+
+        delete_old_events(self.db, self.window_seconds)
 
 
 def setup_database():
     db = sqlite3.connect(':memory:')
-    db.execute(f"create table Events({', '.join(event.field_names())});")
-    db.commit()
+    columns = ', '.join(event.field_names())
+    db.execute(f"create table Events({columns});")
+    db.execute(f"create index Index_Events_unix_time on Events(unix_time)")
     return db
+
+
+def insert_event(db, event: event.Event):
+    columns = ', '.join(event.field_names())
+    placeholders = ', '.join(('?' for _ in columns))
+    values = dataclasses.astuple(event)
+    db.execute(f"insert into Events({columns}) values ({placeholders});", values)
+
+
+def delete_old_events(db, window_seconds: float):
+    # Say we're looking 10 seconds behind, and the timestamps look like this:
+    #
+    #     ..., 4, 4, 5, 6, 6, 8, 8, 8, 9, 12, 15, 16, 16, 17
+    #
+    # 17 - 10 is 7, so we keep 6 and greater.  If there were an event with
+    # timestamp 7, then we'd delete the events with timestamp 6.
+    db.execute("""
+        delete from Events where unix_time <
+            (select max(unix_time)
+            where unix_time <= (select max(unix_time)) - ?);""",
+        window_seconds)
